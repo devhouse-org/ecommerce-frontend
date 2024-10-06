169f6d5b5bab1db4e2790273a8465ea6baf9bbdc
MosaNameer <mosa.mn777@gmail.com>
2024-10-06 16:38:12 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Products from './pages/Products'
 import { MyNavbar } from './components/Navbar'
 import { MyFooter } from './components/Footer'
+import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
       <MyFooter />
     </div>
